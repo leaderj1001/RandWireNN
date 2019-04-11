04aae5bfdf341a53f5b78562d3967f0377736782
myeongjun <leaderj1001@gmail.com>
2019-04-11 17:16:13 +0900

    [Update] SeparableConv2d kernel size

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ from graph import RandomGraph
 # reference, Thank you.
 # https://github.com/tstandley/Xception-PyTorch/blob/master/xception.py
 class SeparableConv2d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, dilation=1, bias=False):
         super(SeparableConv2d, self).__init__()
         self.conv = nn.Conv2d(in_channels, in_channels, kernel_size, stride, padding, dilation, groups=in_channels, bias=bias)
         self.pointwise = nn.Conv2d(in_channels, out_channels, 1, 1, 0, 1, 1, bias=bias)
