32c4432f4a0df433475bb8ba29d3d8672ec9b2b0
myeongjun <leaderj1001@gmail.com>
2019-04-20 13:07:50 +0900

    [Update] remove test code

--- visualize.py
+++ visualize.py
@@ -41,23 +41,3 @@ def just_resize_show(img, width, height, time_interval=2, img_save=False):
     plt.show(block=False)
     plt.pause(time_interval)
     plt.close()
-
-
-# weight 가져오는 부분
-# print(list(model.conv1_2.weight))
-
-# 여러 이미지를 한꺼번에 보여주는 코드
-# fig = plt.figure(figsize=(15, 6))
-# for i in range(10):
-#     subplot = fig.add_subplot(2, 5, i + 1)
-#     subplot.set_xticks([])
-#     subplot.set_yticks([])
-#
-#     if i < 5:
-#         subplot.imshow(np.array(data[i]).reshape((28, 28)))
-#     else:
-#         resize = cv2.resize((p[i - 5].data).cpu().numpy(), (28, 28))
-#         subplot.imshow(resize)
-# plt.show(block=False)
-# plt.pause(1)
-# plt.close()
